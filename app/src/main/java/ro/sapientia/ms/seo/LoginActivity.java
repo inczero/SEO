7c7e2981bee89ee8910457825f42df8d7876c68d
Incze Roland <incze_roland@yahoo.com>
2018-11-06 21:27:08 +0200

    Added Login activity class + layout

---
+++ app/src/main/java/ro/sapientia/ms/seo/LoginActivity.java
@@ -0,0 +1,21 @@
+package ro.sapientia.ms.seo;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+
+public class LoginActivity extends AppCompatActivity {
+
+    //Firebase attributes
+    private FirebaseAuth firebaseAuth;
+    private DatabaseReference databaseReference;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
