e73a9315314a5240067ef8cb3496e5aea9e80014
Incze Roland <incze_roland@yahoo.com>
2018-11-13 16:46:06 +0200

    Added Register with email activity class + layout

--- app/src/main/java/ro/sapientia/ms/seo/RegisterActivity.java
+++ app/src/main/java/ro/sapientia/ms/seo/RegisterActivity.java
@@ -3,13 +3,10 @@ package ro.sapientia.ms.seo;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class RegisterActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_register);
-
-        //test
     }
 }
