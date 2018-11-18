2c83299d05df3016cff583962dba45c3451b6ad0
Incze Roland <incze_roland@yahoo.com>
2018-11-18 23:56:03 +0200

    Implemented registration and login (with email)

--- app/src/main/java/ro/sapientia/ms/seo/LoginActivity.java
+++ app/src/main/java/ro/sapientia/ms/seo/LoginActivity.java
@@ -1,23 +1,123 @@
 package ro.sapientia.ms.seo;
 
+import android.content.Context;
+import android.content.Intent;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseAuthInvalidUserException;
 import com.google.firebase.database.DatabaseReference;
 
 public class LoginActivity extends AppCompatActivity {
 
     //Firebase attributes
     private FirebaseAuth firebaseAuth;
-    private DatabaseReference databaseReference;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
+
+        if (!isOnline()) {
+            setContentView(R.layout.activity_no_network);
+            Button exitButton = findViewById(R.id.exit_button);
+            exitButton.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    finish();
+                }
+            });
+        } else {
+            setContentView(R.layout.activity_login);
+
+            //Layout elements
+            final EditText emailEditText = findViewById(R.id.emailInput);
+            final EditText passwordEditText = findViewById(R.id.passwordInput);
+            Button loginButton = findViewById(R.id.emailLoginButton);
+            Button loginWithPhoneButton = findViewById(R.id.phoneLoginButton);
+            TextView signUpTextView = findViewById(R.id.signUpTextView);
+
+            loginButton.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    String email = emailEditText.getText().toString();
+                    String password = passwordEditText.getText().toString();
+
+                    if (email.isEmpty()) {
+                        emailEditText.setError("Please enter your email address!");
+                        emailEditText.requestFocus();
+                        return;
+                    }
+
+                    if (password.isEmpty()) {
+                        passwordEditText.setError("Please enter your password!");
+                        passwordEditText.requestFocus();
+                        return;
+                    }
+
+                    firebaseAuth = FirebaseAuth.getInstance();
+
+                    firebaseAuth.signInWithEmailAndPassword(email, password).addOnCompleteListener(LoginActivity.this, new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+                            if (task.isSuccessful()) {
+                                startActivity(new Intent(LoginActivity.this, MainActivity.class));
+                            } else {
+                                try {
+                                    throw task.getException();
+                                } catch (FirebaseAuthInvalidUserException e) {
+                                    Toast.makeText(getApplicationContext(), "Invalid user!", Toast.LENGTH_SHORT).show();
+                                } catch (Exception e) {
+                                    Toast.makeText(getApplicationContext(), "Login failed!", Toast.LENGTH_SHORT).show();
+                                }
+                            }
+                        }
+                    });
+                }
+            });
+
+            loginWithPhoneButton.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    Toast.makeText(getApplicationContext(), "Login with phone number is currently not implemented!", Toast.LENGTH_SHORT).show();
+                }
+            });
+
+            signUpTextView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    startActivity(new Intent(LoginActivity.this, RegisterActivity.class));
+                }
+            });
 
 
+
+        }
+
+
+    }
+
+    //Check for internet connection
+    public boolean isOnline() {
+        try {
+            ConnectivityManager connMgr = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+            NetworkInfo networkInfo = connMgr.getActiveNetworkInfo();
+            return networkInfo.isConnected();
+        } catch (NullPointerException e) {
+            return false;
+        }
     }
 }
--- app/src/main/java/ro/sapientia/ms/seo/RegisterActivity.java
+++ app/src/main/java/ro/sapientia/ms/seo/RegisterActivity.java
@@ -1,12 +1,126 @@
 package ro.sapientia.ms.seo;
 
+import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseAuthUserCollisionException;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.ArrayList;
 
 public class RegisterActivity extends AppCompatActivity {
+
+    private FirebaseAuth firebaseAuth;
+    private DatabaseReference databaseReference;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+
+        final TextView firstNameTextView = findViewById(R.id.registerFirstNameInput);
+        final TextView lastNameTextView = findViewById(R.id.registerLastNameInput);
+        final TextView emailTextView = findViewById(R.id.registerEmailInput);
+        final TextView passwordTextView = findViewById(R.id.registerPasswordInput);
+        final TextView reenterPasswordTextView = findViewById(R.id.registerReenterPasswordInput);
+        Button signUpButton = findViewById(R.id.emailSignUpButton);
+
+        firebaseAuth = FirebaseAuth.getInstance();
+        databaseReference = FirebaseDatabase.getInstance().getReference("Users");
+
+        signUpButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                final String firstName = firstNameTextView.getText().toString();
+                final String lastName = lastNameTextView.getText().toString();
+                String email = emailTextView.getText().toString();
+                String password = passwordTextView.getText().toString();
+                String reenterPassword = reenterPasswordTextView.getText().toString();
+
+                if (firstName.isEmpty()) {
+                    firstNameTextView.setError("Please enter your first name!");
+                    firstNameTextView.requestFocus();
+                    return;
+                }
+
+                if (lastName.isEmpty()) {
+                    lastNameTextView.setError("Please enter your last name!");
+                    lastNameTextView.requestFocus();
+                    return;
+                }
+
+                if (email.isEmpty()) {
+                    emailTextView.setError("Please enter your email address!");
+                    emailTextView.requestFocus();
+                    return;
+                }
+
+                if (password.isEmpty()) {
+                    passwordTextView.setError("Please enter a password!");
+                    passwordTextView.requestFocus();
+                    return;
+                }
+
+                if (password.length() < 8) {
+                    passwordTextView.setError("The password must be at least 8 characters long!");
+                    passwordTextView.requestFocus();
+                    return;
+                }
+
+                if (reenterPassword.isEmpty()) {
+                    reenterPasswordTextView.setError("Please enter your password here again!");
+                    reenterPasswordTextView.requestFocus();
+                    return;
+                }
+
+                if (password.compareTo(reenterPassword) != 0) {
+                    reenterPasswordTextView.setError("Your password does not match! Please reenter it!");
+                    reenterPasswordTextView.requestFocus();
+                    return;
+                }
+
+                firebaseAuth.createUserWithEmailAndPassword(email, password).addOnCompleteListener(RegisterActivity.this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (!task.isSuccessful()) {
+                            try {
+                                throw task.getException();
+                            } catch (FirebaseAuthUserCollisionException e) {
+                                emailTextView.setError("This email is already registered!");
+                                emailTextView.requestFocus();
+                            } catch (Exception e) {
+                                Toast.makeText(RegisterActivity.this, "An error happened!", Toast.LENGTH_SHORT).show();
+                            }
+                        } else {
+                            Toast.makeText(RegisterActivity.this, "Successfully registered!", Toast.LENGTH_LONG).show();
+
+                            ArrayList<SmartSocket> ownedProducts = new ArrayList<>();
+
+                            User newUserInformation = new User(firstName, lastName, "", "", ownedProducts);
+
+                            FirebaseUser newUserAuth = FirebaseAuth.getInstance().getCurrentUser();
+                            databaseReference.child(newUserAuth.getUid()).setValue(newUserInformation);
+                            startActivity(new Intent(RegisterActivity.this , MainActivity.class));
+                        }
+                    }
+                });
+
+            }
+        });
+
+
     }
 }
